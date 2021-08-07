908226932e5ba1389fb87c1e4c3f131efee18fbb
MellYu <mel6273569@gmail.com>
2021-08-07 11:47:20 +0300

    added logic for active users

--- my-app/src/components/Users/Users.js
+++ my-app/src/components/Users/Users.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { usersData } from "../../utils/fakeData";
 import { v4 as uuidv4 } from "uuid";
 import arrow from "../../assets/img/button-arrow.svg";
@@ -5,11 +6,43 @@ import "./Users.css";
 
 const buttons = [1, 2, 3, 4, 5];
 export const UsersComponent = () => {
+  const initialData = usersData;
+  const [data, setData] = useState(initialData);
+  const [value, setValue] = useState("Active first");
+  const onHandleChange = (e) => {
+    setValue(e.target.value);
+    setData(usersData);
+  };
+
+  const checkData = (value) => {
+    switch (value) {
+      case "Not active first":
+        const newData = [
+          data.filter((i) => !i.isOnline),
+          data.filter((i) => i.isOnline),
+        ];
+        setData(newData.flat());
+        break;
+      case "Only active":
+        setData(data.filter((i) => i.isOnline && i));
+        break;
+      case "Only not active":
+        setData(data.filter((i) => !i.isOnline && i));
+        break;
+      default:
+        setData(usersData);
+    }
+  };
+
+  useEffect(() => {
+    checkData(value);
+  }, [value]);
+
   return (
     <>
       <div>
-        <h2>Users ({usersData.length})</h2>
-        <select>
+        <h2>Users ({data.length})</h2>
+        <select onChange={onHandleChange}>
           <option value="Active first" defaultValue>
             Active first
           </option>
@@ -30,7 +63,7 @@ export const UsersComponent = () => {
             </tr>
           </thead>
           <tbody>
-            {usersData.map((item) => {
+            {data.map((item) => {
               const { name, position, isOnline, time, email, phone } = item;
               return (
                 <tr className="table-item" key={uuidv4()}>
@@ -51,7 +84,7 @@ export const UsersComponent = () => {
       </div>
       <div>
         {buttons.map((i) => (
-          <button>{i}</button>
+          <button key={uuidv4()}>{i}</button>
         ))}
         <button>
           <img src={arrow} />
